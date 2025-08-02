f6464acce3cf466f2acedd7f4fa944a096f25b22
maroofhaider <creativemaroof@gmail.com>
2025-08-02 17:27:50 +0500

    update

--- public/inc/StyleLinks.php
+++ public/inc/StyleLinks.php
@@ -4,20 +4,21 @@
 <link rel="stylesheet" type="text/css" href="./public/assets/css/owl.carousel.min.css">
 <link rel="stylesheet" type="text/css" href="./public/assets/css/Style.min.css">
 
-<script src="https://www.google.com/recaptcha/enterprise.js?render=6LeF5Q8qAAAAAF_JqZFO65FKvrp_6HB87t5MZ5ka"></script>
+<script src="https://www.google.com/recaptcha/api.js?render=6LeF5Q8qAAAAAF_JqZFO65FKvrp_6HB87t5MZ5ka"></script>
 
-<script>
-    grecaptcha.ready(function() {
-        document.querySelector('.contact-form').addEventListener('submit', function(e) {
-            e.preventDefault();
-            grecaptcha.execute('6LeF5Q8qAAAAAF_JqZFO65FKvrp_6HB87t5MZ5ka', {action: 'submit'}).then(function(token) {
-                let input = document.createElement('input');
-                input.type = 'hidden';
-                input.name = 'g-recaptcha-response';
-                input.value = token;
-                e.target.appendChild(input);
-                e.target.submit();
+
+    <script>
+        grecaptcha.ready(function() {
+            document.querySelector('.contact-form').addEventListener('submit', function(e) {
+                e.preventDefault();
+                grecaptcha.execute('6LeF5Q8qAAAAAF_JqZFO65FKvrp_6HB87t5MZ5ka', {action: 'submit'}).then(function(token) {
+                    let input = document.createElement('input');
+                    input.type = 'hidden';
+                    input.name = 'g-recaptcha-response';
+                    input.value = token;
+                    e.target.appendChild(input);
+                    e.target.submit();
+                });
             });
         });
-    });
-</script>
+    </script>
